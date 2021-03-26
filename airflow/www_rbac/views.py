416aa0dd8f58bc3028d3932cc241d86b6e4386a7
Chao-Han Tsai <milton0825@gmail.com>
2021-03-26 13:39:07 -0700

    Fix dagrun access control (#249)
    * Fix DagRun access control
    
    * add more

--- airflow/www_rbac/views.py
+++ airflow/www_rbac/views.py
@@ -68,7 +68,7 @@ from airflow.utils.state import State
 from airflow._vendor import nvd3
 from airflow.www_rbac import utils as wwwutils
 from airflow.www_rbac.app import app, appbuilder
-from airflow.www_rbac.decorators import action_logging, gzipped, has_dag_access, has_dag_access_ti
+from airflow.www_rbac.decorators import action_logging, gzipped, has_dag_access, has_dag_access_ti_or_dagrun
 from airflow.www_rbac.forms import (DateTimeForm, DateTimeWithNumRunsForm,
                                     DateTimeWithNumRunsWithDagRunsForm,
                                     DagRunForm, ConnectionForm)
@@ -2455,7 +2455,7 @@ class DagRunModelView(AirflowModelView):
 
     @action('muldelete', "Delete", "Are you sure you want to delete selected records?",
             single=False)
-    @has_dag_access(can_dag_edit=True)
+    @has_dag_access_ti_or_dagrun(can_dag_edit=True)
     @provide_session
     def action_muldelete(self, items, session=None):
         self.datamodel.delete_all(items)
@@ -2466,6 +2466,7 @@ class DagRunModelView(AirflowModelView):
         return redirect(self.get_redirect())
 
     @action('set_running', "Set state to 'running'", '', single=False)
+    @has_dag_access_ti_or_dagrun(can_dag_edit=True)
     @provide_session
     def action_set_running(self, drs, session=None):
         try:
@@ -2488,6 +2489,7 @@ class DagRunModelView(AirflowModelView):
     @action('set_failed', "Set state to 'failed'",
             "All running task instances would also be marked as failed, are you sure?",
             single=False)
+    @has_dag_access_ti_or_dagrun(can_dag_edit=True)
     @provide_session
     def action_set_failed(self, drs, session=None):
         try:
@@ -2515,6 +2517,7 @@ class DagRunModelView(AirflowModelView):
     @action('set_success', "Set state to 'success'",
             "All task instances would also be marked as success, are you sure?",
             single=False)
+    @has_dag_access_ti_or_dagrun(can_dag_edit=True)
     @provide_session
     def action_set_success(self, drs, session=None):
         try:
@@ -2647,28 +2650,28 @@ class TaskInstanceModelView(AirflowModelView):
             flash('Failed to set state', 'error')
 
     @action('set_running', "Set state to 'running'", '', single=False)
-    @has_dag_access_ti(can_dag_edit=True)
+    @has_dag_access_ti_or_dagrun(can_dag_edit=True)
     def action_set_running(self, tis):
         self.set_task_instance_state(tis, State.RUNNING)
         self.update_redirect()
         return redirect(self.get_redirect())
 
     @action('set_failed', "Set state to 'failed'", '', single=False)
-    @has_dag_access_ti(can_dag_edit=True)
+    @has_dag_access_ti_or_dagrun(can_dag_edit=True)
     def action_set_failed(self, tis):
         self.set_task_instance_state(tis, State.FAILED)
         self.update_redirect()
         return redirect(self.get_redirect())
 
     @action('set_success', "Set state to 'success'", '', single=False)
-    @has_dag_access_ti(can_dag_edit=True)
+    @has_dag_access_ti_or_dagrun(can_dag_edit=True)
     def action_set_success(self, tis):
         self.set_task_instance_state(tis, State.SUCCESS)
         self.update_redirect()
         return redirect(self.get_redirect())
 
     @action('set_retry', "Set state to 'up_for_retry'", '', single=False)
-    @has_dag_access_ti(can_dag_edit=True)
+    @has_dag_access_ti_or_dagrun(can_dag_edit=True)
     def action_set_retry(self, tis):
         self.set_task_instance_state(tis, State.UP_FOR_RETRY)
         self.update_redirect()
