cec6acd5d2734737c7f92f6e6ca5e80dfa5e1ce4
Paul Lee <paullee@lyft.com>
2022-05-23 13:26:29 -0700

    add v10 changes: https://github.com/apache/airflow/pull/22965/files

--- airflow/providers/google/ads/hooks/ads.py
+++ airflow/providers/google/ads/hooks/ads.py
@@ -16,19 +16,25 @@
 # specific language governing permissions and limitations
 # under the License.
 """This module contains Google Ad hook."""
+import sys
 from tempfile import NamedTemporaryFile
-from typing import IO, Any, Dict, Generator, List
+from typing import IO, Any, Dict, List, Optional
 
-from cached_property import cached_property
-from google.ads.google_ads.client import GoogleAdsClient
-from google.ads.google_ads.errors import GoogleAdsException
-from google.ads.google_ads.v2.types import GoogleAdsRow
+if sys.version_info >= (3, 8):
+    from functools import cached_property
+else:
+    from cached_property import cached_property
+
+from google.ads.googleads.client import GoogleAdsClient
+from google.ads.googleads.errors import GoogleAdsException
+from google.ads.googleads.v10.services.services.customer_service import CustomerServiceClient
+from google.ads.googleads.v10.services.services.google_ads_service import GoogleAdsServiceClient
+from google.ads.googleads.v10.services.types.google_ads_service import GoogleAdsRow, SearchGoogleAdsRequest
 from google.api_core.page_iterator import GRPCIterator
 from google.auth.exceptions import GoogleAuthError
-from googleapiclient.discovery import Resource
 
 from airflow import AirflowException
-from airflow.hooks.base_hook import BaseHook
+from airflow.hooks.base import BaseHook
 
 
 class GoogleAdsHook(BaseHook):
@@ -46,12 +52,12 @@ class GoogleAdsHook(BaseHook):
             {
                 "google_ads_client": {
                     "developer_token": "{{ INSERT_TOKEN }}",
-                    "path_to_private_key_file": null,
-                    "delegated_account": "{{ INSERT_DELEGATED_ACCOUNT }}"
+                    "json_key_file_path": null,
+                    "impersonated_email": "{{ INSERT_IMPERSONATED_EMAIL }}"
                 }
             }
 
-        The ``path_to_private_key_file`` is resolved by the hook using credentials from gcp_conn_id.
+        The ``json_key_file_path`` is resolved by the hook using credentials from gcp_conn_id.
         https://developers.google.com/google-ads/api/docs/client-libs/python/oauth-service
 
     .. seealso::
@@ -63,42 +69,112 @@ class GoogleAdsHook(BaseHook):
         https://developers.google.com/google-ads/api/docs/start
 
     :param gcp_conn_id: The connection ID with the service account details.
-    :type gcp_conn_id: str
     :param google_ads_conn_id: The connection ID with the details of Google Ads config.yaml file.
-    :type google_ads_conn_id: str
+    :param api_version: The Google Ads API version to use.
 
     :return: list of Google Ads Row object(s)
     :rtype: list[GoogleAdsRow]
     """
 
+    default_api_version = "v10"
+
     def __init__(
         self,
+        api_version: Optional[str],
         gcp_conn_id: str = "google_cloud_default",
         google_ads_conn_id: str = "google_ads_default",
-        api_version: str = "v3",
     ) -> None:
-        
+        super().__init__()
+        self.api_version = api_version or self.default_api_version
         self.gcp_conn_id = gcp_conn_id
         self.google_ads_conn_id = google_ads_conn_id
-        self.gcp_conn_id = gcp_conn_id
-        self.api_version = api_version
         self.google_ads_config: Dict[str, Any] = {}
 
+    def search(
+        self, client_ids: List[str], query: str, page_size: int = 10000, **kwargs
+    ) -> List[GoogleAdsRow]:
+        """
+        Pulls data from the Google Ads API and returns it as native protobuf
+        message instances (those seen in versions prior to 10.0.0 of the
+        google-ads library).
+
+        This method is for backwards compatibility with older versions of the
+        google_ads_hook.
+
+        Check out the search_proto_plus method to get API results in the new
+        default format of the google-ads library since v10.0.0 that behave
+        more like conventional python object (using proto-plus-python).
+
+        :param client_ids: Google Ads client ID(s) to query the API for.
+        :param query: Google Ads Query Language query.
+        :param page_size: Number of results to return per page. Max 10000.
+        :return: Google Ads API response, converted to Google Ads Row objects
+        :rtype: list[GoogleAdsRow]
+        """
+        data_proto_plus = self._search(client_ids, query, page_size, **kwargs)
+        data_native_pb = [row._pb for row in data_proto_plus]
+
+        return data_native_pb
+
+    def search_proto_plus(
+        self, client_ids: List[str], query: str, page_size: int = 10000, **kwargs
+    ) -> List[GoogleAdsRow]:
+        """
+        Pulls data from the Google Ads API and returns it as proto-plus-python
+        message instances that behave more like conventional python objects.
+
+        :param client_ids: Google Ads client ID(s) to query the API for.
+        :param query: Google Ads Query Language query.
+        :param page_size: Number of results to return per page. Max 10000.
+        :return: Google Ads API response, converted to Google Ads Row objects
+        :rtype: list[GoogleAdsRow]
+        """
+        return self._search(client_ids, query, page_size, **kwargs)
+
+    def list_accessible_customers(self) -> List[str]:
+        """
+        Returns resource names of customers directly accessible by the user authenticating the call.
+        The resulting list of customers is based on your OAuth credentials. The request returns a list
+        of all accounts that you are able to act upon directly given your current credentials. This will
+        not necessarily include all accounts within the account hierarchy; rather, it will only include
+        accounts where your authenticated user has been added with admin or other rights in the account.
+
+        ..seealso::
+            https://developers.google.com/google-ads/api/reference/rpc
+
+        :return: List of names of customers
+        """
+        try:
+            accessible_customers = self._get_customer_service.list_accessible_customers()
+            return accessible_customers.resource_names
+        except GoogleAdsException as ex:
+            for error in ex.failure.errors:
+                self.log.error('\tError with message "%s".', error.message)
+                if error.location:
+                    for field_path_element in error.location.field_path_elements:
+                        self.log.error('\t\tOn field: %s', field_path_element.field_name)
+            raise
+
     @cached_property
-    def _get_service(self) -> Resource:
+    def _get_service(self) -> GoogleAdsServiceClient:
         """Connects and authenticates with the Google Ads API using a service account"""
+        client = self._get_client
+        return client.get_service("GoogleAdsService", version=self.api_version)
+
+    @cached_property
+    def _get_client(self) -> GoogleAdsClient:
         with NamedTemporaryFile("w", suffix=".json") as secrets_temp:
             self._get_config()
             self._update_config_with_secret(secrets_temp)
             try:
                 client = GoogleAdsClient.load_from_dict(self.google_ads_config)
-                return client.get_service("GoogleAdsService", version=self.api_version)
+                return client
             except GoogleAuthError as e:
                 self.log.error("Google Auth Error: %s", e)
                 raise
 
     @cached_property
-    def _get_customer_service(self) -> Resource:
+    def _get_customer_service(self) -> CustomerServiceClient:
         """Connects and authenticates with the Google Ads API using a service account"""
         with NamedTemporaryFile("w", suffix=".json") as secrets_temp:
             self._get_config()
@@ -132,38 +208,42 @@ class GoogleAdsHook(BaseHook):
         secrets_temp.write(secret)
         secrets_temp.flush()
 
-        self.google_ads_config["path_to_private_key_file"] = secrets_temp.name
+        self.google_ads_config["json_key_file_path"] = secrets_temp.name
 
-    def search(
+    def _search(
         self, client_ids: List[str], query: str, page_size: int = 10000, **kwargs
     ) -> List[GoogleAdsRow]:
         """
         Pulls data from the Google Ads API
 
         :param client_ids: Google Ads client ID(s) to query the API for.
-        :type client_ids: List[str]
         :param query: Google Ads Query Language query.
-        :type query: str
         :param page_size: Number of results to return per page. Max 10000.
-        :type page_size: int
 
         :return: Google Ads API response, converted to Google Ads Row objects
         :rtype: list[GoogleAdsRow]
         """
         service = self._get_service
-        iterators = (
-            service.search(client_id, query=query, page_size=page_size, **kwargs) for client_id in client_ids
-        )
+
+        iterators = []
+        for client_id in client_ids:
+            request = self._get_client.get_type("SearchGoogleAdsRequest")  # type: SearchGoogleAdsRequest
+            request.customer_id = client_id
+            request.query = query
+            request.page_size = page_size
+
+            iterator = service.search(request=request)
+            iterators.append(iterator)
+
         self.log.info("Fetched Google Ads Iterators")
 
         return self._extract_rows(iterators)
 
-    def _extract_rows(self, iterators: Generator[GRPCIterator, None, None]) -> List[GoogleAdsRow]:
+    def _extract_rows(self, iterators: List[GRPCIterator]) -> List[GoogleAdsRow]:
         """
         Convert Google Page Iterator (GRPCIterator) objects to Google Ads Rows
 
         :param iterators: List of Google Page Iterator (GRPCIterator) objects
-        :type iterators: generator[GRPCIterator, None, None]
 
         :return: API response for all clients in the form of Google Ads Row object(s)
         :rtype: list[GoogleAdsRow]
@@ -182,28 +262,4 @@ class GoogleAdsHook(BaseHook):
                 if error.location:
                     for field_path_element in error.location.field_path_elements:
                         self.log.error("\t\tOn field: %s", field_path_element.field_name)
-            raise
-
-    def list_accessible_customers(self) -> List[str]:
-        """
-        Returns resource names of customers directly accessible by the user authenticating the call.
-        The resulting list of customers is based on your OAuth credentials. The request returns a list
-        of all accounts that you are able to act upon directly given your current credentials. This will
-        not necessarily include all accounts within the account hierarchy; rather, it will only include
-        accounts where your authenticated user has been added with admin or other rights in the account.
-
-        ..seealso::
-            https://developers.google.com/google-ads/api/reference/rpc
-
-        :return: List of names of customers
-        """
-        try:
-            accessible_customers = self._get_customer_service.list_accessible_customers()
-            return accessible_customers.resource_names
-        except GoogleAdsException as ex:
-            for error in ex.failure.errors:
-                self.log.error('\tError with message "%s".', error.message)
-                if error.location:
-                    for field_path_element in error.location.field_path_elements:
-                        self.log.error('\t\tOn field: %s', field_path_element.field_name)
             raise
--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def do_setup():
         install_requires=[
             'apache-airflow~=1.10',
             'PyOpenSSL',
-            'google-ads>=4.0.0,<8.0.0',
+            'google-ads>=15.1.1',
             'google-api-core>=1.25.1,<2.0.0',
             'google-api-python-client>=1.6.0,<2.0.0',
             'google-auth-httplib2>=0.0.1',
