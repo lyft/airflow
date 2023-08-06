79a777009fa2e252d082947007c13bd9d6764538
eschachar <eschachar@lyft.com>
2023-08-05 20:22:21 -0700

    Fix space

--- airflow/models/dagbag.py
+++ airflow/models/dagbag.py
@@ -116,6 +116,7 @@ class DagBag(LoggingMixin):
 
         dag_folder = dag_folder or settings.DAGS_FOLDER
         self.dag_folder = dag_folder
+
         self.service_instance = os.environ.get('SERVICE_INSTANCE', '').lower()
 
         if self.service_instance == "production":
